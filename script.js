6ede2daeae4d69613cfdf64459a681d6f848284e
ClushAKvK <90634189+ClushAKvK@users.noreply.github.com>
2021-12-16 17:50:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,105 @@
-function newPrice() {
-  let s = document.getElementsByName("productType");
+function updatePrice() {
+
+  let s = document.getElementsByName("prodType");
   let select = s[0];
-  let price = 0, prices = getPrices();
+  let price = 0;
+  let prices = getPrices();
   let priceIndex = parseInt(select.value) - 1;
-  if (priceIndex >= 0) { price = prices.productTypes[priceIndex]; }
-  count = document.getElementById('productCount').value;
-
-  let checkDiv = document.getElementById("checkboxes");
-  checkDiv.style.display = (select.value == "2" ? "block" : "none");
-
+  if (priceIndex >= 0) {
+    price = prices.prodTypes[priceIndex];
+	
+  }
+  count = document.getElementById('prodCount').value;
+  
   let radioDiv = document.getElementById("radios");
   radioDiv.style.display = (select.value == "3" ? "block" : "none");
 
-  let priceProp = 0;
-  let checkboxes = document.querySelectorAll("#checkboxes input");
-  checkboxes.forEach(function(checkbox) {
-    if (checkbox.checked) {
-      let propPrice = prices.productProperties[checkbox.name];
-      if (propPrice !== undefined) {
-        priceProp += propPrice;
-      }
-    }
-  });
+  let checkDiv = document.getElementById("checkboxes");
+  checkDiv.style.display = (select.value == "2" ? "block" : "none"); 
 
   let priceOption = 0;
-  let radios = document.getElementsByName("productOptions");
+  let radios = document.getElementsByName("prodOptions");
   radios.forEach(function(radio) {
     if (radio.checked) {
-      let optionPrice = prices.productOptions[radio.value];
+      let optionPrice = prices.prodOptions[radio.value];
       if (optionPrice !== undefined) {
         priceOption += optionPrice;
       }
     }
   });
 
-  let productPrice = document.getElementById("productPrice");
-  productPrice.innerHTML = price * count + (select.value == "3" ? priceOption : 0) + (select.value == "2" ? priceProp : 0) + " руб.";
+  let priceProp = 0;
+  let checkboxes = document.querySelectorAll("#checkboxes input");
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      let propPrice = prices.prodProperties[checkbox.name];
+      if (propPrice !== undefined) {
+        priceProp += propPrice;
+      }
+    }
+  });
+  
+  let prodPrice = document.getElementById("prodPrice");
+  prodPrice.innerHTML = price * count + (select.value == "3" ? priceOption : 0) + (select.value == "2" ? priceProp : 0) + " руб.";
+}
+
+function getPrices() {
+  return {
+    prodTypes: [1000, 100000, 5000],
+    prodOptions: {
+      option1: 1000,
+      option2: 2000,
+      option3: 20000,
+    },
+    prodProperties: {
+      prop1: 1000,
+      prop2: 3000,
+    }
+  };
 }
 
 window.addEventListener('DOMContentLoaded', function (event) {
+
   let radioDiv = document.getElementById("radios");
   radioDiv.style.display = "none";
-
-  let s = document.getElementsByName("productType");
+  
+  let s = document.getElementsByName("prodType");
   let select = s[0];
+
   select.addEventListener("change", function(event) {
     let target = event.target;
     console.log(target.value);
-    newPrice();
+    updatePrice();
   });
-
-  let radios = document.getElementsByName("productOptions");
+  
+  let radios = document.getElementsByName("prodOptions");
   radios.forEach(function(radio) {
     radio.addEventListener("change", function(event) {
       let r = event.target;
       console.log(r.value);
-      newPrice();
+      updatePrice();
     });
   });
-
+	
   let checkboxes = document.querySelectorAll("#checkboxes input");
   checkboxes.forEach(function(checkbox) {
     checkbox.addEventListener("change", function(event) {
       let c = event.target;
       console.log(c.name);
       console.log(c.value);
-      newPrice();
+      updatePrice();
     });
   });
-
-  let productCount = document.querySelectorAll("#productCount input");
-  productCount.forEach(function(count) {
+  
+  let prodCount = document.querySelectorAll("#prodCount input");
+  prodCount.forEach(function(count) {
     count.addEventListener("change", function(event) {
       let d = event.target;
       console.log(d.value);
 	  console.log(d.name);
-      newPrice();
+      updatePrice();
     });
   });
 
-  newPrice();
+  updatePrice();
 });
-
-function getPrices() {
-  return {
-    productTypes: [1000, 500000, 5000],
-    productOptions: {
-      option1: 1000,
-      option2: 7000,
-      option3: 10000,
-    },
-    productProperties: {
-      prop1: 15000,
-      prop2: 120000,
-      prop3: 7000,
-    }
-  };
-}
